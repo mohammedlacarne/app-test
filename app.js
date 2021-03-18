746eb9485eb001ff45973660bbfb18871024c7c3
mohammedlacarne <mohammed.lacarne@homadata.fr>
2021-03-18 10:27:06 +0100

    test

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const http = require('http')
 const express = require("express")
 const router = express()
-//const dotenv = require('dotenv')
-//dotenv.config()
+const dotenv = require('dotenv')
+dotenv.config()
 
 
 
 router.get('/', async(req, res, next) =>{
   res.send('hello')
 })
-//const port = process.env.SERVER_PORT || '4000';
-router.set('port', "3000");
+const port = process.env.SERVER_PORT || '4000';
+router.set('port', port);
 const server = http.createServer(router);
 /**
   * Listen on provided port, on all network interfaces.
 */
-server.listen("3000");
+server.listen(port, function(error) {
+    if (error) {
+      console.error("Unable to listen on port", port, error);
+      return;}
+    });
